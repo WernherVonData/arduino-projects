16e5ce45f177e6f4c9860600a263fe5393b6b175
danpeczek <danpeczek@gmail.com>
2023-04-19 21:58:11 +0200

    wishes are drafted

--- gift5-year/gift5-year.ino
+++ gift5-year/gift5-year.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
+#include <Fonts/Org_01.h>
 
 Adafruit_PCD8544 display{7, 6, 5, 4, 3};
 
@@ -13,8 +14,6 @@ void setup()
   Serial.println("Initializing...");
   display.begin();
   display.setContrast(60);
-  display.display();
-  delay(100);
   display.clearDisplay();
   display.display();
 }
@@ -22,13 +21,57 @@ void setup()
 void loop()
 {
   display.clearDisplay();
-  display.setTextSize(2);
   display.setTextColor(BLACK);
+
+  display.setTextSize(2);
   display.setCursor(0, 0);
-  // display.println("Kochana");
-  // display.println("Eluniu!");
-  display.println("Good");
-  display.println("Night!");
+  display.println("Kochana");
+  display.println("Eluniu!");
   display.display();
-  delay(2000);
+  delay(5000);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("Ostatnie 5 lat");
+  display.println("");
+  display.println("Bylo");
+  display.println("najlepszym...");
+  display.display();
+  delay(5000);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("Okresem");
+  display.println("");
+  display.println("Mojego zycia");
+  display.display();
+  delay(5000);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("Kocham Ciebie");
+  display.println("");
+  display.println("i Kasie");
+  display.display();
+  delay(5000);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println("Bardzo");
+  display.println("Mocno!");
+  display.display();
+  delay(5000);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println("I chce zawsze");
+  display.println("");
+  display.println("byc z Toba");
+  display.display();
+  delay(5000);
 }
