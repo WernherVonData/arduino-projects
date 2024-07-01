26d793610a12a4d85357de6069308ee7ed63d792
wernhervondata <danpeczek@gmail.com>
2024-07-01 21:40:14 +0200

    works with max7219

--- WeatherStation/weather_station/weather_station.ino
+++ WeatherStation/weather_station/weather_station.ino
@@ -9,7 +9,11 @@
 
 #define HUMIDITY_LINE 0x02
 #define TEMPERATURE_LINE 0x04
+#define TEMPERATURE_LINE_2 0x05
+
 #define PRESSURE_LINE 0x07
+#define PRESSURE_LINE_2 0x08
+#define STANDARD_ATM 1013
 
 namespace
 {
@@ -22,6 +26,8 @@ void setup()
 {
   pinMode(CS_PIN, OUTPUT);
   SPI.begin();
+  sendData(0x0C, 0x00); // Shutdown: 1 (normal operation)
+  delay(2000);
   sendData(0x09, 0x00); // Decode mode: none
   sendData(0x0A, 0x0F); // Intensity: 3 (0x00 to 0x0F)
   sendData(0x0B, 0x07); // Scan limit: 7 (all digits)
@@ -60,16 +66,18 @@ void loop()
     Serial.print(dht_humidity);
     Serial.println("%");
     Serial.print("Temperature: ");
-    float temparature = (dht_temp + bmp_temp) / 2.0;
-    Serial.print(temparature);
+    float temperature = (dht_temp + bmp_temp) / 2.0;
+    Serial.print(temperature);
     Serial.println("°C");
     Serial.print("Pressure ");
     Serial.print(bmp_pressure);
     Serial.println(" hPa");
     Serial.println("*****");
-    Serial.println((byte)dht_humidity);
     sendData(HUMIDITY_LINE, (byte)dht_humidity);
-    sendData(TEMPERATURE_LINE, (byte)temparature);
+    sendData(TEMPERATURE_LINE, (byte)temperature);
+    sendData(TEMPERATURE_LINE_2, (byte)getTwoDecimals(temperature));
+    float pressure_diff = STANDARD_ATM - (int)(bmp_pressure);
+    sendData(PRESSURE_LINE, (byte)pressure_diff);
     delay(1000);
   }
 }
@@ -79,4 +87,9 @@ void sendData(byte register_address, byte value) {
   SPI.transfer(register_address);
   SPI.transfer(value);
   digitalWrite(CS_PIN, HIGH);
+}
+
+int getTwoDecimals(float value_to_change) {
+  int value = (value_to_change - (int) value_to_change) * 100;
+  return (value / 10 % 10 * 10) + (value % 10);
 }
